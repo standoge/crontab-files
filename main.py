f5d520a8a871c192f04de0f664941f6ccfdf6156
StanDoge <StanlyDoge@gmail.com>
2022-02-12 00:48:03 -0600

    working with os module only

--- main.py
+++ main.py
@@ -1,43 +1,40 @@
 import os
-import shutil
 import re
 
 workspace = "/mnt/c/Users/kevin/Downloads"
-files = "/mnt/c/Users/kevin/Downloads/files"
-pdfs = "/mnt/c/Users/kevin/Downloads/pdfs"
-imgs = "/mnt/c/Users/kevin/Downloads/immms"
+files = "/mnt/c/Users/kevin/Downloads/micelaneus"
+pdf = "/mnt/c/Users/kevin/Downloads/pdfs"
+imgs = "/mnt/c/Users/kevin/Downloads/images"
 pattern_pdf = re.compile(r"[a-z\ ]*(\.pdf)")
-pattern_img = re.compile(r"[a-z\ ]*(\.jpg|\.png)")
+pattern_img = re.compile(r"[a-z\ ]*(\.jpg|\.png|\.jpeg)")
+
 
 def recollect():
-	with os.scandir(workspace) as sentinel:
-		for e in sentinel:
-			move_dir(e) if e.is_file() else print(f"dxr -> {e.name}") 
+    with os.scandir(workspace) as sentinel:
+        for e in sentinel:
+            move_dir(e) if e.is_file() else print(f"dxr -> {e.name}")
 
 
 def move_dir(file):
-	one = re.search(pattern_pdf,file.name)
-	two = re.search(pattern_img,file.name)
+    one = re.search(pattern_pdf, file.name)
+    two = re.search(pattern_img, file.name)
 
-	if one:
-		shutil.move(file.path,pdfs)
+    if one:
+		# shutil.move(file.path, pdf)
+        os.rename(file.path, pdf + '/' + file.name)
 
-	elif two:
-		shutil.move(file.path,imgs)
+    elif two:
+        # shutil.move(file.path, imgs)
+        os.rename(file.path, imgs + '/' + file.name)
 
-	else:
-		print(one)
-	# 	shutil.move(file.path , files)
-
-def test(string):
-	print("uwu") if re.match(pattern_img, string) else print("todo aml")
+    else:
+        print(one)
+    # 	shutil.move(file.path , files)
 
 
 def main():
-	recollect()
+    recollect()
 
 
 if __name__ == '__main__':
     main()
-
-
