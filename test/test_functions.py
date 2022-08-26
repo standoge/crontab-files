1a2551a51b18babbad7db25ace5b94fe23ef09f5
StanDoge <stanlydoge@gmail.com>
2022-08-25 18:51:39 -0600

    Test for directories()

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,8 +1,14 @@
 import pytest
+import os
 from functions import log
+from functions import LOGS_PATH, SNAPSHOTS, FILES, DOCS, IMGS, WORKSPACE
 
 class TestFunctions():
 
 	def test_log():
 		assert type(log()) == str , "log() must return a str"
 		print("log() works")
+
+	def test_directories():
+		assert os.path.exists(LOGS_PATH) , "Function doesn't create LOGS_PATH"
+		print("Function creates LOGS_PATH")
