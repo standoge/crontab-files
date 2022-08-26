c90ddafa762b87aea0e561421571c6c73df68110
StanDoge <stanlydoge@gmail.com>
2022-08-25 20:47:02 -0600

    More assert statements in test

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 from src.functions import log
-from src.functions import LOGS_PATH, SNAPSHOTS, FILES, DOCS, IMGS, WORKSPACE
+from src.functions import LOGS_PATH, SNAPSHOTS, FILES, DOCS, IMGS 
 
 class TestFunctions():
 
@@ -11,4 +11,8 @@ class TestFunctions():
 
 	def test_directories(self):
 		assert os.path.exists(LOGS_PATH) , "Function doesn't create LOGS_PATH"
-		print("Function creates LOGS_PATH")
+		assert os.path.exists(SNAPSHOTS) , "Function doesn't create SNAPSHOTS"
+		assert os.path.exists(FILES) , "Function doesn't create FILES"
+		assert os.path.exists(DOCS) , "Function doesn't create DOCS"
+		assert os.path.exists(IMGS) , "Function doesn't create IMGS"
+		print("All dirs was created")
