1deb6ba21380f3b5694bbcb427469459c098d5bd
StanDoge <stanlydoge@gmail.com>
2022-08-25 20:32:33 -0600

    Add self in methods :p

--- test/test_functions.py
+++ test/test_functions.py
@@ -5,10 +5,10 @@ from src.functions import LOGS_PATH, SNAPSHOTS, FILES, DOCS, IMGS, WORKSPACE
 
 class TestFunctions():
 
-	def test_log():
+	def test_log(self):
 		assert type(log()) == str , "log() must return a str"
 		print("log() works")
 
-	def test_directories():
+	def test_directories(self):
 		assert os.path.exists(LOGS_PATH) , "Function doesn't create LOGS_PATH"
 		print("Function creates LOGS_PATH")
